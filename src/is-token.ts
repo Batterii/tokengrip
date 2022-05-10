8607d51282aaad35376f200746c272a11bbf5453
Steve Ripberger <sripberger@batterii.com>
2022-05-10 15:57:08 -0400

    Add docs for isToken

--- src/is-token.ts
+++ src/is-token.ts
@@ -1,6 +1,11 @@
 import {decodeObject, InvalidJsonError} from "@batterii/encode-object";
 import {is} from "nani";
 
+/**
+ * Checks whether a string is a Tokengrip token.
+ * @param str The string to check.
+ * @returns `true` if the string is a Tokengrip token, false otherwise.
+ */
 export function isToken(str: string): boolean {
 	const [header] = str.split(".");
 	if (!header) return false;
